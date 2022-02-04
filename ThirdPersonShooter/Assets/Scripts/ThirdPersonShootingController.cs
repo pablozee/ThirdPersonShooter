e107778c253ee43cc917a12be40296b26609d7e7
Zeshan Rasul <zeshanrasul@gmail.com>
2022-02-04 01:21:51 +0000

    Third Person Shooting Controller WIP

--- ThirdPersonShooter/Assets/Scripts/ThirdPersonShootingController.cs
+++ ThirdPersonShooter/Assets/Scripts/ThirdPersonShootingController.cs
@@ -7,6 +7,8 @@ using Cinemachine;
 public class ThirdPersonShootingController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private GameObject debugTransform;
 
     private PlayerInputActions inputActions;
     private bool aim;
@@ -25,9 +27,25 @@ public class ThirdPersonShootingController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.transform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+
         if (aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 10f);
         } else
         {
             aimVirtualCamera.gameObject.SetActive(false);
